04d20534f36a163aeb9ca59eb0070efc677ad163
PurumPumPum <42623281+PurumPumPum@users.noreply.github.com>
2018-08-28 23:25:46 +0300

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoTrimps-Zek
 // @version      1.0-Zek
-// @namespace    https://Zorn192.github.io/AutoTrimps
-// @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
+// @namespace    https://purumpumpum.github.io/Trimps/
+// @updateURL    https://purumpumpum.github.io/Trimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
 // @include      *trimps.github.io*
@@ -14,9 +14,9 @@
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
+script.id = 'AutoTrimps-GR';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://purumpumpum.github.io/Trimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
